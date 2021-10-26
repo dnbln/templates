189c6ae232a892f1bb21aad881a95f850e4b57e8
Dinu Blanovschi <dinu.blanovschi@criptext.com>
2021-10-26 13:08:42 +0300

    Fix env-commons.nix setup

--- nix/env-commons.nix
+++ nix/env-commons.nix
@@ -7,14 +7,31 @@
 }:
 
 let
-tt = toolchain-utils.targetTripleEnv target;
+  tt = toolchain-utils.targetTripleEnv target;
+  useNightlyOpts = isNightly && enableNightlyOpts;
+
+  linkerRustFlags = lld: lib.optionals lld [ "-Clink-arg=-fuse-ld=lld" ];
+
+  nightlyRustFlags = nightlyOpts: lib.optionals nightlyOpts [ "-Zshare-generics=y" ];
+
+  rustFlags = { lld, nightlyOpts }: linkerRustFlags lld ++ nightlyRustFlags nightlyOpts;
+  rustFlagsStr = { lld, nightlyOpts }: builtins.concatStringsSep " " (rustFlags { inherit lld nightlyOpts; });
+
+  cargoSetupList = [
+    ''export CARGO_BUILD_TARGET="${target}"''
+  ]
+  ++ lib.optionals uselld [
+    ''export CARGO_TARGET_${tt}_LINKER="clang"''
+  ]
+  ++ [
+    ''export CARGO_TARGET_${tt}_RUSTFLAGS="${
+        rustFlagsStr {
+          lld = uselld;
+          nightlyOpts = useNightlyOpts;
+        }
+      }"''
+  ];
 in
 {
-  setup = ''
-    export CARGO_BUILD_TARGET="${target}"
-  ''
-  + (builtins.concatStringsSep "\n" (lib.optionals (isNightly && enableNightlyOpts) [
-    ''export CARGO_TARGET_${tt}_LINKER="clang"''
-    ''export CARGO_TARGET_${tt}_RUSTFLAGS="-Clink-arg=-fuse-ld=lld -Zshare-generics=y"''
-  ]));
+  setup = builtins.concatStringsSep "\n" cargoSetupList;
 }
